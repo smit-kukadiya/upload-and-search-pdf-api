aa3f6f37d9c3d9dae5479677a3036f3f635d0e03
Smit Kukadiya <smitk85143@gmail.com>
2023-08-17 17:28:15 +0530

    Done

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+S3_LOCATION = 'http://s3-eu-north-1.amazonaws.com/'
+S3_KEY = 'AKIATTNVDKJQL7GRJD6Q'
+S3_SECRET = '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'
+S3_UPLOAD_DIRECTORY = 'uploads'
+S3_BUCKET = 'upload-and-search'
+
+SECRET_KEY = "FLASK_SECRET_KEY"
+DEBUG = True
+PORT = 5000
